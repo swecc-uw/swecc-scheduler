48c4aa0f273619d157ce61972ea01c1f51780c49
Elijah Melton <elimelt@uw.edu>
2024-11-28 18:17:12 -0800

    health every 5 min

--- schedule/tasks.sh
+++ schedule/tasks.sh
@@ -2,8 +2,8 @@
 
 source "$(dirname "$0")/../config/settings.sh"
 
-# every minute
-health_check_schedule="* * * * *"
+# every 5 minute
+health_check_schedule="*/5 * * * *"
 # bash -c to ensure date is evaluated at runtime
 health_check_command="bash -c 'echo \"[\$(date +\"%Y-%m-%d %H:%M:%S\")] [HEALTH] \$(curl $BASE_URL/health/)\" >> $LOG_DIR/health_check.log'"
 
