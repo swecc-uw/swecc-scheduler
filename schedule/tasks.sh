fdff821d2e9479f297c59550d466675359d7ca84
Elijah Melton <98496503+elimelt@users.noreply.github.com>
2024-11-29 06:07:29 +0000

    add leaderboard tasks
    brought to you by Elijah's IPhone

--- schedule/tasks.sh
+++ schedule/tasks.sh
@@ -4,8 +4,15 @@ source "$(dirname "$0")/../config/settings.sh"
 
 # every 5 minute
 health_check_schedule="*/5 * * * *"
-# bash -c to ensure date is evaluated at runtime
 health_check_command="bash -c 'echo \"[\$(date +\"%Y-%m-%d %H:%M:%S\")] [HEALTH] \$(curl $BASE_URL/health/)\" >> $LOG_DIR/health_check.log'"
 
+# every 6 hours
+update_leetcode_stats_schedule="0 */6 * * *"
+update_leetcode_stats_command="curl -X POST -H 'Content-Type: application/json' -d '{\"command\": \"update_leetcode_stats\"}' $BASE_URL/admin/commands"
+
+# every 6 hours, offset by 3 hours
+update_github_stats_schedule="0 3-23/6 * * *"
+update_github_stats_command="curl -X POST -H 'Content-Type: application/json' -d '{\"command\": \"update_github_stats\"}' $BASE_URL/admin/commands"
+
 # export all tasks
-TASKS="health_check"
+TASKS="health_check update_leetcode_stats update_github_stats"
