e880eef2c1b3fd1226a994c47b3d3a805c49c41b
Elijah Melton <elimelt@uw.edu>
2024-11-29 20:24:33 -0800

    .

--- schedule/tasks.sh
+++ schedule/tasks.sh
@@ -8,11 +8,11 @@ health_check_command="curl -s -S -f \"$BASE_URL/health/\" -o /dev/null || echo \
 
 # every 6 hours
 update_leetcode_stats_schedule="0 */6 * * *"
-update_leetcode_stats_command="curl -s -S -f -X POST -H 'Content-Type: application/json' -H 'Authorization: ${API_KEY}' -d '{\"command\": \"update_leetcode_stats\"}' \"$BASE_URL/admin/command/\""
+update_leetcode_stats_command="curl -s -S -f -X POST -H 'Content-Type: application/json' -H 'Authorization: Api-Key ${API_KEY}' -d '{\"command\": \"update_leetcode_stats\"}' \"$BASE_URL/admin/command/\""
 
 # every 6 hours, offset by 3 hours
 update_github_stats_schedule="0 3-23/6 * * *"
-update_github_stats_command="curl -s -S -f -X POST -H 'Content-Type: application/json' -H 'Authorization: $API_KEY' -d '{\"command\": \"update_github_stats\"}' \"$BASE_URL/admin/command/\""
+update_github_stats_command="curl -s -S -f -X POST -H 'Content-Type: application/json' -H 'Authorization: Api-Key $API_KEY' -d '{\"command\": \"update_github_stats\"}' \"$BASE_URL/admin/command/\""
 
 # export all tasks
 TASKS="health_check update_leetcode_stats update_github_stats"
